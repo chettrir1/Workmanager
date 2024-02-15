89444aa58636face653c62e1d24619a314370cc9
Raju Chettri <dada@Rajus-MacBook-Pro.local>
2024-02-15 13:27:36 -0500

    viewmodel file added

--- app/src/main/java/com/raju/workmanager/MainActivity.kt
+++ app/src/main/java/com/raju/workmanager/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.raju.workmanager
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.work.Constraints
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
 import com.raju.workmanager.ui.theme.WorkmanagerTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var workManager: WorkManager
+    private val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        workManager = WorkManager.getInstance(applicationContext)
         setContent {
             WorkmanagerTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,25 +30,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val workerResult = viewModel.workId?.let { id ->
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WorkmanagerTheme {
-        Greeting("Android")
+        val uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent?.getParcelableExtra(Intent.EXTRA_STREAM, Uri::class.java)
+        } else {
+            intent?.getParcelableExtra(Intent.EXTRA_STREAM)
+        } ?: return
+
+        viewModel.updateUncompressedUri(uri)
+
+        val request = OneTimeWorkRequestBuilder<PhotoCompressionWorker>()
+            .setInputData(
+                workDataOf(
+                    PhotoCompressionWorker.KEY_CONTENT_URI to uri.toString(),
+                    PhotoCompressionWorker.KEY_COMPRESSION_THRESHOLD to 1024 * 20L
+                )
+            )
+            .setConstraints(
+                Constraints(
+                    requiresStorageNotLow = true
+                )
+            )
+            .build()
+        viewModel.updateWorkId(request.id)
+        workManager.enqueue(request)
     }
 }
