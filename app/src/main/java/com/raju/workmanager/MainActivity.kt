ed626d248036cfbd68652f7ec38f522828b077b5
Raju Chettri <dada@Rajus-MacBook-Pro.local>
2024-02-16 11:52:20 -0500

    image compressor and view added

--- app/src/main/java/com/raju/workmanager/MainActivity.kt
+++ app/src/main/java/com/raju/workmanager/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.raju.workmanager
 
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.unit.dp
 import androidx.work.Constraints
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.workDataOf
+import coil.compose.AsyncImage
 import com.raju.workmanager.ui.theme.WorkmanagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,6 +44,33 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     val workerResult = viewModel.workId?.let { id ->
+                        workManager.getWorkInfoByIdLiveData(id).observeAsState().value
+                    }
+                    LaunchedEffect(key1 = workerResult?.outputData) {
+                        if (workerResult?.outputData != null) {
+                            val filePath = workerResult.outputData.getString(
+                                PhotoCompressionWorker.KEY_RESULT_PATH
+                            )
+                            filePath?.let {
+                                val bitmap = BitmapFactory.decodeFile(it)
+                                viewModel.updateCompressedBitmap(bitmap)
+                            }
+                        }
+                    }
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        viewModel.uncompressedUri?.let {
+                            Text(text = "Uncompressed Photo :")
+                            AsyncImage(model = it, contentDescription = null)
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
+                        viewModel.compressedBitmap?.let {
+                            Text(text = "Compressed Photo :")
+                            Image(bitmap = it.asImageBitmap(), contentDescription = null)
+                        }
                     }
                 }
             }
